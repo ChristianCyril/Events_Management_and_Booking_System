f047fc6f995c5c34cf4dc20c8f54f399430386a2
ChristianCyril <christiancyril78@gmail.com>
2026-03-08 17:28:10 +0100

    implementation of the verifyJWT and verifyRoles middlewares

---
+++ Backend/middleware/verifyJWT.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const verifyJWT = (req,res,next)=>{
+  const authHeader = req.headers.authorization || req.headers.Authorization;;
+  if(!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+  const token = authHeader.split(' ')[1];
+  //verify token
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (error,decoded)=>{
+      if(error) return res.sendStatus(403);
+      req.role = decoded.role;
+      req.id = decoded.id;
+      next();
+    }
+  );
+}
+
+export default verifyJWT;
