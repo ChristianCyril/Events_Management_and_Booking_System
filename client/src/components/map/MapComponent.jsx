bcfaa66b41c8a049eae93eaefe3e621b127eed7d
ChristianCyril <christiancyril78@gmail.com>
2026-03-12 23:19:12 +0100

    created the MapComponent

---
+++ client/src/components/map/MapComponent.jsx
@@ -0,0 +1,34 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
+import "./MapComponent.css"
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+})
+
+export default function MapComponent({ lat, lng, address }) {
+  if (!lat || !lng) return null
+
+  return (
+    <div className="map-wrapper">
+      <MapContainer
+        key={`${lat}-${lng}`}
+        center={[lat, lng]}
+        zoom={15}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]}>
+          <Popup>{address}</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
+}
